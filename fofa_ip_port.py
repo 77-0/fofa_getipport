8c29257b371ecee1d850febf7710aef48198daff
x1anyv <39888034+moyvting@users.noreply.github.com>
2021-08-31 16:01:30 +0800

    Update fofa_ip_port.py

--- fofa_ip_port.py
+++ fofa_ip_port.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from os import WIFSTOPPED
 import client
 import json
 import re
@@ -27,46 +26,58 @@ output = {"ip": "", "port": [], "len": ""}
 login = client.Client(email, key)
 
 for quest in query_str:
-    datas = login.get_data(quest, fields=fields)
-    if datas['size'] >= 10000:
-        page = 100 + 1
-    else:
-        page = datas['size'] / 100 + 2
 
-    for page in range(1, int(page)):
-        datas = login.get_data(
-            quest, fields="ip,port", page=page)  # 获取所有数据
-        for data in datas["results"]:
-            content['data'].append(data)
+    if page == "":
+        datas = login.get_data(quest, fields=fields)
+        if datas['size'] >= 10000:
+            page = 100 + 1
+        else:
+            page = datas['size'] / 100 + 2
+
+        for page in range(1, int(page)):
+            datas = login.get_data(
+                quest, fields="ip,port", page=page)  # 获取所有数据
+            for data in datas["results"]:
+                content['data'].append(data)
+    else:
+        for page in range(1, int(page)+1):
+            datas = login.get_data(
+                quest, fields="ip,port", page=page)
+            for data in datas["results"]:
+                content['data'].append(data)
+
+
 
 if content["data"] == []:
     del content["data"]
     content["error"] = "not found"
-    content = json.dumps(content)
+    print(content["error"])
+else:
 
-data_dict = {}
-for data in content["data"]:
-    if data[0] in data_dict.keys():
-        if data[1] not in data_dict[data[0]]:
-            data_dict[data[0]].append(data[1])
-    else:
-        data_dict[data[0]] = [data[1]]
-num = 0
+    data_dict = {}
 
-del content["data"]
+    for data in content["data"]:
+        if data[0] in data_dict.keys():
+            if data[1] not in data_dict[data[0]]:
+                data_dict[data[0]].append(data[1])
+        else:
+            data_dict[data[0]] = [data[1]]
+    num = 0
 
-for data in data_dict.keys():
-    content["data" + str(num)] = {"ip": data,"port": data_dict[data], "len": data_dict[data].__len__()}
-    num += 1
+    del content["data"]
 
-print(content)
+    for data in data_dict.keys():
+        content["data" + str(num)] = {"ip": data,"port": data_dict[data], "len": data_dict[data].__len__()}
+        num += 1
 
-with open('ip_port.txt' ,"w+") as f :
-    f.write("")
+    print(content)
+
+    with open('ip_port.txt' ,"w+") as f :
+        f.write("")
 
 
-for key in content.keys():
-    IP = content[key]["ip"]
-    for port  in content[key]["port"]:
-        with open('ip_port.txt' ,"a+") as f :
-            f.write( str(IP) +":"+  str(port)  + "\n")
+    for key in content.keys():
+        IP = content[key]["ip"]
+        for port  in content[key]["port"]:
+            with open('ip_port.txt' ,"a+") as f :
+                f.write( str(IP) +":"+  str(port)  + "\n")
